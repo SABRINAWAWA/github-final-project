dca6f3073beebc2d508146f070f8ecffcf887e9a
Huiying-Sabrina <53490593+SABRINAWAWA@users.noreply.github.com>
2023-03-19 17:53:25 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Huiying Sabrina Wang
    # Additional Authors:
    # <your GitHub username>
 
